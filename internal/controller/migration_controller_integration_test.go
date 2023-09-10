32cdfe0af8c01dd29b37948d082de51edd327c48
David J. M. Karlsen <david@davidkarlsen.com>
2023-09-10 16:27:29 +0200

    more teststuff

--- internal/controller/migration_controller_integration_test.go
+++ internal/controller/migration_controller_integration_test.go
@@ -5,6 +5,7 @@ import (
 	flywayv1alpha1 "github.com/davidkarlsen/flyway-operator/api/v1alpha1"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
@@ -45,28 +46,26 @@ var _ = Describe("Migration controller", func() {
 						},
 						JdbcUrl: "jdbc:db2://somehost:50000/SOME_DB",
 					},
+					MigrationSource: flywayv1alpha1.MigrationSource{
+						ImageRef: "ghcr.io/davidkarlsen/testmigration:latest",
+					},
 				},
 			}
 
 			Expect(k8sClient.Create(ctx, migration)).Should(Succeed())
-			Eventually(func() bool {
+			Eventually(func(g Gomega) {
 				createdMigration := &flywayv1alpha1.Migration{}
 				objectKey := types.NamespacedName{Namespace: migration.Namespace, Name: migration.Name}
 				err := k8sClient.Get(ctx, objectKey, createdMigration)
-				if err != nil {
-					return false
-				}
-				/*
-					job := &batchv1.Job{}
-					err = k8sClient.Get(ctx, objectKey, job)
-					if err != nil {
-						return false
-					}
+				g.Expect(err).To(BeNil())
 
-					//TODO: asserts
-				*/
-				return true
-			}, timeout, interval).Should(BeTrue())
+				job := &batchv1.Job{}
+				err = k8sClient.Get(ctx, objectKey, job)
+
+				// TODO: fix
+				// g.Expect(err).To(BeNil())
+
+			}, timeout, interval).Should(Succeed())
 		})
 	})
 })
