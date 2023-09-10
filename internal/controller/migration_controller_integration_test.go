1cf4c7c62b40a518653bbda5452bc7daf63df1dd
David J. M. Karlsen <david@davidkarlsen.com>
2023-09-10 14:43:15 +0200

    add integration tests

---
+++ internal/controller/migration_controller_integration_test.go
@@ -0,0 +1,56 @@
+package controller
+
+import (
+	"context"
+	flywayv1alpha1 "github.com/davidkarlsen/flyway-operator/api/v1alpha1"
+	"github.com/onsi/ginkgo/v2"
+	"github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+var _ = ginkgo.Describe("Migration controller", func() {
+
+	const (
+		namespace = "default"
+		name      = "test"
+	)
+
+	ginkgo.Context("When creating a migration", func() {
+		ginkgo.It("Should create a job", func() {
+			ginkgo.By("By creating a new migration")
+			ctx := context.Background()
+			migration := &flywayv1alpha1.Migration{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: flywayv1alpha1.GroupVersion.Version,
+					Kind:       "Migration",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Namespace: namespace,
+					Name:      name,
+				},
+				Spec: flywayv1alpha1.MigrationSpec{
+					Database: flywayv1alpha1.Database{
+						Username: "someUser",
+						Credentials: corev1.SecretKeySelector{
+							LocalObjectReference: corev1.LocalObjectReference{
+								Name: "somesecret",
+							},
+							Key: "someKey",
+						},
+						JdbcUrl: "jdbc:db2://somehost:50000/SOME_DB",
+					},
+				},
+			}
+
+			createdMigration := &flywayv1alpha1.Migration{}
+
+			gomega.Expect(k8sClient.Create(ctx, migration)).Should(gomega.Succeed())
+			gomega.Eventually(func() bool {
+				err := k8sClient.Get(ctx, types.NamespacedName{migration.Namespace, migration.Name}, createdMigration)
+				return err == nil
+			}).Should(gomega.BeTrue())
+		})
+	})
+})
